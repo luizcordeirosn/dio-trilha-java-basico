638b7bbdb55c85dba8196e897be8e42288d670b4
Luiz Neto <luizcsneto@outlook.com>
2024-08-05 09:49:16 -0300

    Criação da classe ContaBanco recebendo dados via terminal contendo as características de conta em banco como Agência, Número da Conta, Nome Cliente e Saldo

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Agora digite o número de sua conta: ");
+        int numeroConta = Integer.parseInt(scanner.nextLine());
+
+        System.out.println("Digite o seu nome: ");
+        String nomeCliente = scanner.nextLine().toUpperCase();
+
+        System.out.println("Digite o saldo de sua conta: ");
+        Float saldo = Float.parseFloat(scanner.nextLine());
+
+        System.out.printf("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
