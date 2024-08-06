109ca608ab8b7beb496ccb6ee69b72d010b0e3d3
Luiz Neto <luizcsneto@outlook.com>
2024-08-06 14:50:36 -0300

    Criação de método para gerar um salário aleatório entre 1800 e 2200 para um candidato durante um processo de seleção

--- ControleCandidatos/src/candidatura/ProcessoSeletivo.java
+++ ControleCandidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,38 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 
     public static void main(String[] args) {
         System.out.println("Processo Seletivo");
-        analisarCandidato(1900.0);
-        analisarCandidato(2200.0);
-        analisarCandidato(2000.0);
+
+        selecaoCandidatos();
+    }
+
+    static void selecaoCandidatos() {
+
+        String[] candidatos = { "Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela",
+                "Daniela", "Jorge" };
+
+        int candidatosSelecionados = 0;
+        int candidatosAtual = 0;
+        double salarioBase = 2000.0;
+
+        while (candidatosSelecionados < 5 && (candidatosAtual < candidatos.length)) {
+            String candidato = candidatos[candidatosAtual];
+            double salarioPretendido = valorPretendido();
+            System.out.println("O candidato " + candidato + " solicitou este valor de salário de " + salarioPretendido);
+            if (salarioBase >= salarioPretendido) {
+                System.out.println("O candidato " + candidato + " foi selecionado para a vaga");
+                candidatosSelecionados++;
+            }
+            candidatosAtual++;
+        }
+    }
+
+    static double valorPretendido() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
     }
 
     static void analisarCandidato(double salarioPretendido) {
