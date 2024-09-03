f0f01862c69b29bf2a61b9b13b5790da5310558f
Luiz Neto <luizcsneto@outlook.com>
2024-09-03 15:23:31 -0300

    feat: adicionar testes unitários para calcular idade e verificar maioridade na classe Pessoa

---
+++ junit5-exemplos/src/test/java/PessoaTest.java
@@ -0,0 +1,17 @@
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import com.junit.entities.Pessoa;
+
+public class PessoaTest {
+    
+    @Test
+    void deveCalcularIdadeCorretamente() {
+        Pessoa pessoa = new Pessoa("Luiz", 
+            LocalDateTime.of(2000, 2, 13, 0, 0, 0));
+        
+        Assertions.assertEquals(24, pessoa.getIdade());
+        Assertions.assertTrue(pessoa.ehMaiorDeIdade());
+    }
+}
